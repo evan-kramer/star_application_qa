f77cb41e565ab632172a166c5b843b83287d2e9e
Kramer <evan.kramer@dc.gov>
2019-11-05 18:53:52 -0500

    Added `summarize()` to group errors by Notes comment and automated update into Google sheet.

--- star_app_qa.R
+++ star_app_qa.R
@@ -26,15 +26,52 @@ for(ws in sort(unique(names_ws$`Primary UAT Analyst`))) {
     mutate_all("as.character") %>% 
     filter(`Primary UAT Sign-off` == "Error Found") %>% 
     bind_rows(., star_uat_errors)
-  Sys.sleep(5) # make the machine wait so it doesn't get throttled by the API for excessive requests
+  Sys.sleep(6) # make the machine wait so it doesn't get throttled by the API for excessive requests
 }
 star_uat_errors = mutate(star_uat_errors, Notes = ifelse(!is.na(Notes), Notes, X14)) %>% 
-  select(-X14, -ends_with("_1"))
+  select(-X14, -ends_with("_1")) %>% 
+  arrange(`LEA ID`, `School ID`, `Framework (or Report Card)`, `Metric Name`, `Subgroup Name (Limited to STAR subgroups and All Report Card Students)`)
 
-# Add to Errors sheet
-gs_edit_cells(
-  gs_title("STAR Application UAT"),
-  ws = "Errors",
-  input = star_uat_errors,
-  trim = T
+# Output errors (then add to Google sheet)
+write_csv(
+  star_uat_errors, 
+  str_c("C:/Users/evan.kramer/Downloads/star_uat_errors (", now(), ").csv"), 
+  na = ""
 )
+
+# Compare initial file with current errors
+anti_join(
+star_uat_errors,
+  gs_read(gs_title("STAR Application UAT"), ws = "Errors") %>% 
+    mutate_all("as.character"),
+  by = names(star_uat_errors)
+)
+
+# Add to Errors sheet -- takes forever
+# gs_edit_cells(
+#   gs_title("STAR Application UAT"),
+#   ws = "Errors",
+#   input = star_uat_errors,
+#   trim = T
+# )
+
+# Alternative way to add an Errors sheet -- also takes forever
+if("Errors" %in% gs_ws_ls(gs_title("STAR Application UAT"))) {
+  gs_ws_delete(gs_title("STAR Application UAT"), ws = "Errors")
+}
+gs_ws_new(
+  gs_title("STAR Application UAT"),
+  ws_title = "Errors",
+  row_extent = nrow(star_uat_errors),
+  col_extent = ncol(star_uat_errors),
+
+  gs_edit_cells(
+    ss = gs_title("STAR Application UAT"),
+    ws = "Errors",
+    input = group_by(star_uat_errors, Notes) %>% 
+      summarize_all("first") %>% 
+      ungroup()
+    # input = head(star_uat_errors, 200)
+  )
+)
+
