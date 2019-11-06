61d1adfbe3ec7349c33d35c09cb40fb4f5f735e9
Kramer <evan.kramer@dc.gov>
2019-11-06 16:30:08 -0500

    Created `Error Review` sheet

--- star_app_qa.R
+++ star_app_qa.R
@@ -64,6 +64,7 @@ write_csv(
 )
 
 # Extract errors from sheet before overwriting, just in case
+Sys.sleep(6)
 write_csv(
   gs_read(gs_title("Report Card/STAR Application UAT"), ws = "Errors"),
   str_c(
@@ -75,26 +76,85 @@ write_csv(
 )
 
 # Add additional rows to bottom of Errors sheet
-gs_edit_cells(
-  ss = gs_title("Report Card/STAR Application UAT"),
-  ws = "Errors",
-  input = filter(errors_sheet, is.na(`EK Notes`)) %>% 
-    group_by(Notes) %>% 
-    summarize_all(first) %>% 
-    ungroup(),
-  anchor = str_c("A", gs_read(gs_title("Report Card/STAR Application UAT"), ws = "Errors") %>% nrow() + 1),
-  col_names = F,
-)
+# Sys.sleep(6)
+# gs_edit_cells(
+#   ss = gs_title("Report Card/STAR Application UAT"),
+#   ws = "Errors",
+#   input = filter(errors_sheet, is.na(`EK Notes`)) %>% 
+#     group_by(Notes) %>% 
+#     summarize_all(first) %>% 
+#     ungroup(),
+#   anchor = str_c("A", gs_read(gs_title("Report Card/STAR Application UAT"), ws = "Errors") %>% nrow() + 1),
+#   col_names = F,
+# )
 
 # Summary? 
 group_by(errors_sheet, `EK Notes`) %>% 
   summarize_all(n_distinct) %>% 
   ungroup() %>% 
   rename_all(funs(str_c("# Distinct ", .))) %>% 
-  filter(!is.na(`# Distinct EK Notes`)) %>% 
-  View()
+  filter(!is.na(`# Distinct EK Notes`))
 
-# Create Errors (All) sheet
+# Create Error Review sheet
+arrange(errors_sheet, `LEA ID`, `School Name`, `Framework (or Report Card)`, `Metric Name`, `Subgroup Name (Limited to STAR subgroups and All Report Card Students)`) %>% 
+  rename(Category = `EK Notes`) %>% 
+  mutate(
+    Reviewer = case_when(
+      `LEA ID` == "1" & `School ID` %in% c("318", "442", "450") ~ sort(names_ws$`Primary UAT Analyst`)[1],
+      `LEA ID` == "1" & `School ID` %in% c("454") ~ sort(names_ws$`Primary UAT Analyst`)[3],
+      `LEA ID` == "1" & `School ID` %in% c("884") ~ sort(names_ws$`Primary UAT Analyst`)[4],
+      `LEA ID` == "1" & `School ID` %in% c("LEA") & (str_detect(`Metric Name`, "ELA") | str_detect(`Metric Name`, "Math")) ~ sort(names_ws$`Primary UAT Analyst`)[5],
+      `LEA ID` == "1" & `School ID` %in% c("LEA") & !(str_detect(`Metric Name`, "ELA") | str_detect(`Metric Name`, "Math")) ~ sort(names_ws$`Primary UAT Analyst`)[11],
+      `LEA ID` == "119" & `School ID` %in% c("126") ~ sort(names_ws$`Primary UAT Analyst`)[6],
+      `LEA ID` == "120" & `School ID` %in% c("LEA") ~ sort(names_ws$`Primary UAT Analyst`)[7],
+      `LEA ID` %in% c("129") & `School ID` %in% c("LEA", "209") ~ sort(names_ws$`Primary UAT Analyst`)[7],
+      `LEA ID` %in% c("133", "143", "145", "168")  ~ sort(names_ws$`Primary UAT Analyst`)[8],
+      `LEA ID` == "178" ~ sort(names_ws$`Primary UAT Analyst`)[9],
+      `LEA ID` == "186" & `School ID` %in% c("267") ~ sort(names_ws$`Primary UAT Analyst`)[10],
+      `LEA ID` == "188" ~ sort(names_ws$`Primary UAT Analyst`)[11],
+      T ~ "Evan"
+    ),
+    `Error Type` = NA # Data validation options will be Existing, New
+  ) %>% 
+  arrange(Reviewer) %>% 
+  write_csv(
+    str_c(
+      "C:/Users/evan.kramer/Downloads/star_uat_error_review (", 
+      str_replace_all(now(), "[:-]", ""),
+      ").csv"
+    ), 
+    na = ""
+  )
+
+
+
+
+
+# Alternative way to add an Errors sheet -- also takes forever
+# And how to make sure we don't overwrite existing data
+# if("Errors (All)" %in% gs_ws_ls(gs_title("STAR Application UAT"))) {
+#   gs_ws_delete(gs_title("Report Card/STAR Application UAT"), ws = "Errors (All)")
+# }
+# gs_ws_new(
+#   gs_title("Report Card/STAR Application UAT"),
+#   ws_title = "Errors (All)",
+#   row_extent = group_by(star_uat_errors, Notes) %>%
+#     summarize_all("first") %>%
+#     ungroup() %>%
+#     nrow(),
+#   col_extent = ncol(star_uat_errors) + 1,
+#   gs_edit_cells(
+#     ss = gs_title("Report Card/STAR Application UAT"),
+#     ws = "Errors",
+#     input = group_by(star_uat_errors, Notes) %>%
+#       summarize_all("first") %>%
+#       ungroup() %>%
+#       mutate(`EK Notes` = NA)
+#     # input = head(star_uat_errors, 200)
+#   )
+# )
+
+
 # Assign each one to one of the reviewers
 # Data validate (known/unknown)
 
@@ -117,26 +177,3 @@ group_by(errors_sheet, `EK Notes`) %>%
 #   trim = T
 # )
 
-# Alternative way to add an Errors sheet -- also takes forever
-# And how to make sure we don't overwrite existing data
-# if("Errors" %in% gs_ws_ls(gs_title("STAR Application UAT"))) {
-#   gs_ws_delete(gs_title("Report Card/STAR Application UAT"), ws = "Errors")
-# }
-# gs_ws_new(
-#   gs_title("Report Card/STAR Application UAT"),
-#   ws_title = "Errors",
-#   row_extent = group_by(star_uat_errors, Notes) %>% 
-#     summarize_all("first") %>% 
-#     ungroup() %>% 
-#     nrow(),
-#   col_extent = ncol(star_uat_errors) + 1,
-#   gs_edit_cells(
-#     ss = gs_title("Report Card/STAR Application UAT"),
-#     ws = "Errors",
-#     input = group_by(star_uat_errors, Notes) %>% 
-#       summarize_all("first") %>% 
-#       ungroup() %>% 
-#       mutate(`EK Notes` = NA)
-#     # input = head(star_uat_errors, 200)
-#   )
-# )
